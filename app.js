dcb796ffb4b51f7f07c03f78b5359c7599ef983f
Jordan Hanna <jhanna01@uw.edu>
2013-05-23 23:33:42 -0400

    Fixed the problem of accessing files outside of the directory for index.html using app.use. May not be the best fix, will look into after.

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-// Set up express and create a server.
+/**
+  * Module Dependecies
+  */
+
 var express = require('express');
 var http = require('http');
 var neo4j = require('neo4j');
 var path = require('path');
 
+// Config
+
 var app = express();
 var server = http.createServer(app);
-var port = 3000;
+var port = 3010;
+app.use(express.logger());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'controller')));
+app.use(express.static(path.join(__dirname, 'model')));
+
+
+
 
 var d3 = require("d3");
 
 // Connect to the Neo4j DB
-var db = new neo4j.GraphDatabase('http://localhost:7474');
+//var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 // Log use to console
-app.use(express.logger());
+
 
 app.get('/', function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/plain"})
     res.send("Hello World!");
 }); 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
