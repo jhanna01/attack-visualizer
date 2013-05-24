2031e4b7915f4754a7cb1717f8d25564eefb3d99
Jordan Hanna <jhanna01@uw.edu>
2013-05-23 23:35:22 -0400

    Added 2 comments

--- app.js
+++ app.js
@@ -6,26 +6,24 @@ var express = require('express');
 var http = require('http');
 var neo4j = require('neo4j');
 var path = require('path');
+var d3 = require("d3");
 
 // Config
 
 var app = express();
 var server = http.createServer(app);
 var port = 3010;
+// Log use to console
 app.use(express.logger());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'controller')));
 app.use(express.static(path.join(__dirname, 'model')));
 
 
-
-
-var d3 = require("d3");
-
 // Connect to the Neo4j DB
 //var db = new neo4j.GraphDatabase('http://localhost:7474');
 
-// Log use to console
+
 
 
 app.get('/', function(req, res) {
